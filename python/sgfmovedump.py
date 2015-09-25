508cc61758e5fd6f033a4b6c10ee35e7130ae34b
Clark B. Wierda <cbwierda@gmail.com>
2015-09-25 01:25:04 -0400

    Cleaned up code in sgfmovedump.py
    Ran PyLint on sgfmovedump.py
    Removed dead code
    Removed unneeded/unused variables

--- python/sgfmovedump.py
+++ python/sgfmovedump.py
@@ -7,19 +7,26 @@ Derived from show_sgf.py in gomill examples
 import sys
 from optparse import OptionParser
 
-from gomill import ascii_boards
 from gomill import sgf
 from gomill import sgf_moves
 
 def human_point(column, row):
+    """Return the human-readable version of the point."""
     column_array = "ABCDEFGHJKLMNOPQRSTUVWXYZ"
-    
+
     return column_array[column] + str(row + 1)
 
 def show_sgf_file(pathname, move_number):
-    f = open(pathname)
-    sgf_src = f.read()
-    f.close()
+    """
+    Print the moves of the leftmost branch in the first game in the file.
+
+    Arguments:
+    pathname -- path of SGF file
+    move_number -- optional move at which to stop printing moves
+    """
+    file_handle = open(pathname)
+    sgf_src = file_handle.read()
+    file_handle.close()
     try:
         sgf_game = sgf.Sgf_game.from_string(sgf_src)
         handicap = sgf_game.get_handicap()
@@ -27,9 +34,9 @@ def show_sgf_file(pathname, move_number):
         raise StandardError("bad sgf file")
 
     try:
-        board, plays = sgf_moves.get_setup_and_moves(sgf_game)
-    except ValueError, e:
-        raise StandardError(str(e))
+        _, plays = sgf_moves.get_setup_and_moves(sgf_game)
+    except ValueError, err:
+        raise StandardError(str(err))
     if move_number is not None:
         move_number = max(0, move_number)
         plays = plays[:move_number]
@@ -37,41 +44,38 @@ def show_sgf_file(pathname, move_number):
     current_move_number = 0
     root_node = sgf_game.get_root()
     if handicap != None:
-	print "Handicap is", handicap
-	print "Handicap stones are"
-	handicap_stones = root_node.get('AB')
-	for row, col in handicap_stones:
-	    print human_point(col, row),
-	current_move_number += 1
-	print
-	print "Moves"
-	print "1:".rjust(4), "B ----",
+        print "Handicap is", handicap
+        print "Handicap stones are"
+        handicap_stones = root_node.get('AB')
+        for row, col in handicap_stones:
+            print human_point(col, row),
+        current_move_number += 1
+        print
+        print "Moves"
+        print "1:".rjust(4), "B ----",
     for colour, move in plays:
-	if (current_move_number % 2) == 0:
-	    print
-	current_move_number += 1
+        if (current_move_number % 2) == 0:
+            print
+        current_move_number += 1
         if move is None:
-	    print str(current_move_number).rjust(3) + ":", colour.upper(), "Pass",
+            print str(current_move_number).rjust(3) + ":", colour.upper(),\
+	    "Pass",
             continue
         row, col = move
-        print str(current_move_number).rjust(3) + ":", colour.upper(), human_point(col, row).ljust(4),
-        try:
-            board.play(row, col, colour)
-        except ValueError:
-            raise StandardError("illegal move in sgf file")
-
-#    print ascii_boards.render_board(board)
+        print str(current_move_number).rjust(3) + ":", colour.upper(),\
+        human_point(col, row).ljust(4),
     print
 
-_description = """\
+DESCRIPTION = """\
 Show the moves from an SGF file. If a move number is specified, the moves will be
 shown up to that move number.
 """
 
 def main(argv):
+    """Process the command line arguments and dispatch show_sgf_file()"""
     parser = OptionParser(usage="%prog <filename> [move number]",
-                          description=_description)
-    opts, args = parser.parse_args(argv)
+                          description=DESCRIPTION)
+    _, args = parser.parse_args(argv)
     if not args:
         parser.error("not enough arguments")
     pathname = args[0]
@@ -86,8 +90,8 @@ def main(argv):
         move_number = None
     try:
         show_sgf_file(pathname, move_number)
-    except Exception, e:
-        print >>sys.stderr, "sgfmovedump:", str(e)
+    except StandardError, err:
+        print >>sys.stderr, "sgfmovedump:", str(err)
         sys.exit(1)
 
 if __name__ == "__main__":
